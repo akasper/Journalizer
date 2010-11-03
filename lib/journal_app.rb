65c66e4ce58c62e6721605fe89d866034ee929bf
Andrew Kasper <kasper@vitrue.com>
2010-11-03 12:57:57 -0400

    Some early abstractions of notifications into private methods. Added TODOs to README.

--- lib/journal_app.rb
+++ lib/journal_app.rb
@@ -10,41 +10,48 @@ class JournalApp
   end
   
   def run
-    puts "To quit, enter 'Q' at the command prompt, or type [Ctrl + C]"
-    while true
-      self.prompt
-      self.read
-      self.write
-      self.wait
-    end
+    STDOUT.sync = true
+    puts "To quit, enter 'Q' at the command prompt, or type [Ctrl + C]."
+    #Reminder thread
+    reminder = Thread.new { while true; wait; prompt; end }
+    main     = Thread.new { while true; read; write; end }
+    reminder.join
+    main.join
   end
   
   def prompt
-    `growlnotify Journal --message "Time for a journal entry!"`
-    print "\nWhat are you working on right now? > "
+    growl_notify("Time for a journal entry.")
   end
   
   def read
-    @input = gets
-    exit if @input =~ /^\s*q\s*$/i
+    print " > "; @input = gets
+    exit if @input =~ /^\s*(q|exit)\s*$/i
   end
   
   def write
     @journal << Entry.new(@input, Time.now)
+    growl_notify("Next reminder at #{(Time.now + wait_time).strftime('%H:%M')}.")
   end
   
   def wait
-    wait_time = @interval - Time.now.to_i % @interval
-    puts "Next entry at #{(Time.now + wait_time).strftime('%H:%M')}."
     `sleep #{wait_time.to_s}`
   end  
+  
+  private
+  def growl_notify(message)
+    `growlnotify Journal --message "#{message}"`
+  end
+  
+  def wait_time
+    @interval - Time.now.to_i % @interval
+  end
 end
  
 @app = JournalApp.new
 END {puts "\nExiting..."; @app.journal.save!}
 begin
   @app.run
-#When the user hits Ctrl + C, exit a bit more gracefully...my
+#When the user hits Ctrl + C, exit a bit more gracefully...
 rescue SignalException => e
   exit
 end
