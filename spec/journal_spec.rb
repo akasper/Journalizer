5b6890ef6e84eec11057521f90919e7dec705ee9
Andrew Kasper <kasper@vitrue.com>
2010-11-05 11:35:02 -0400

    Uncommented cleanup code.

--- spec/journal_spec.rb
+++ spec/journal_spec.rb
@@ -1,7 +1,8 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'journal')
 describe Journal do
-  HALLOWEEN = Time.parse('2010-10-31 13:13:13')
-  NEW_YEAR  = Time.parse('2010-01-01')
+  HALLOWEEN = Time.parse('2010-10-31T13:13:13')
+  NEW_YEAR  = Time.parse('2010-01-01T00:00:00')
+  NEW_YEAR_NOON = Time.parse('2010-01-01T12:00:00')
   
   DEFAULT_OUTPUT_PATH = File.join(File.expand_path('.'), 'spec', 'tmp', '.journal')
   
@@ -11,7 +12,7 @@ describe Journal do
   
   describe '#entries' do
     it 'is a valid method' do
-      @journal.should be_respond_to(:entries)      
+      @journal.should be_respond_to(:entries)
     end
     it 'returns a collection' do
       @journal.entries.should be_a(Array)
@@ -19,7 +20,8 @@ describe Journal do
     it 'sorts entries by time' do
       @journal << (@entry1 = Entry.new('I\'m spooked out!', HALLOWEEN))
       @journal << (@entry2 = Entry.new('I am making resolutions', NEW_YEAR))
-      @journal.entries.should == [@entry2, @entry1]
+      @journal << (@entry3 = Entry.new('I am hung over.', NEW_YEAR_NOON))
+      @journal.entries.should == [@entry2, @entry3, @entry1] 
     end
   end
   
@@ -48,8 +50,8 @@ describe Journal do
     end
   end
   
-  # after :each do 
-  #   `rm -rf #{DEFAULT_OUTPUT_PATH}`
-  # end
+  after :each do 
+    `rm -rf #{DEFAULT_OUTPUT_PATH}`
+  end
   
 end
