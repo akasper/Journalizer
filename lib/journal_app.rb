4d966fbffce8b2b3a403d344e0ecaf03d7b5dfc7
Andrew Kasper <andrew.m.kasper@gmail.com>
2017-01-30 11:55:37 -0500

    Get Journal app running in modern OSX

--- lib/journal_app.rb
+++ lib/journal_app.rb
@@ -1,25 +1,26 @@
 #!/usr/bin/env ruby
-require 'readline'
+require 'terminal-notifier'
 require File.join(File.dirname(__FILE__), 'journal')
 
 class JournalApp
-  POLLING_INTERVAL = 15.0
+  POLLING_INTERVAL = 1.0
+  DEFAULT_REMINDER_INTERVAL = 15*60
   INSTRUCTIONS = "To quit, enter 'Q' at the command prompt, or type [Ctrl + C]."
+
+  include TerminalNotifier
   attr_accessor :input, :next_notification
   attr_reader :interval, :journal
-  
-  def initialize(interval=30)
+
+  def initialize(interval=DEFAULT_REMINDER_INTERVAL)
     @interval = interval
     @next_notification = Time.now + @interval
     @journal = Journal.new
   end
-  
+
   def run
     STDOUT.sync = true
     puts INSTRUCTIONS
-
-    main_thread.join
-    reminder_thread.join
+    [reminder_thread, main_thread].map(&:join)
   end
 
   def reminder_thread
@@ -29,46 +30,38 @@ class JournalApp
   def main_thread
     Thread.new { while true; read; write; end }
   end
-  
-  def notify
-    growl_notify("Time for a journal entry.")
-  end
-  
+
   def read
     @input = Readline.readline(' > ', true)
     exit if ( !@input || @input =~ /^\s*(q|exit)\s*$/i )
   end
-  
+
   def write
     @journal << Entry.new(@input, Time.now)
-    
-    #we make this the #write's responsibility to ensure that a 
+
+    #we make this the #write's responsibility to ensure that a
     #user is not prompted if he has already written an entry
     #during the last interval
     update_notification
-    growl_notify("Next reminder at #{(@next_notification).strftime('%H:%M')}.")
+    notify("Next reminder at #{(@next_notification).strftime('%H:%M')}.")
   end
-  
+
   def wait
     while true
-      if Time.now > @next_notification      
+      if Time.now > @next_notification
         update_notification
-        notify
+        notify("Time for a journal entry.")
       end
       sleep POLLING_INTERVAL
     end
-  end  
-  
-private
-  def growl_notify(message)
-    `growlnotify Journal --message "#{message}"`
   end
 
+private
   def update_notification
     @next_notification = Time.now + @interval
   end
 end
- 
+
 @app = JournalApp.new
 END {puts "\nExiting..."; @app.journal.save!; exit}
 begin
@@ -77,4 +70,3 @@ begin
 rescue SignalException => e
   exit
 end
-
